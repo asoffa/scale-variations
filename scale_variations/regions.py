3b43f9d6f5f64dc1d83a367b7c5bfe7278fb60fa
Aaron Soffa <asoffa@uci.edu>
2016-06-18 05:49:08 -0700

    clean up

--- scale_variations/regions.py
+++ scale_variations/regions.py
@@ -2,7 +2,7 @@
 from collections import OrderedDict
 
 
-SRs      = OrderedDict()  # (scale factor, region cut)
+SRs      = OrderedDict()
 SRs_reco = OrderedDict()
 
 
@@ -13,7 +13,7 @@ def n_lep_cut(n_leptons, is_reco=False):
         if n_leptons < 2:
             return "0"
         elif n_leptons == 2:
-            return "((n_leptons > 2 || isSS) && lepton_pt[1] > 20)".format(n_leptons)
+            return "((n_leptons > 2 || (n_leptons == 2 && isSS)) && lepton_pt[1] > 20)".format(n_leptons)
         else:
             return "(n_leptons >= {} && lepton_pt[1] > 20)".format(n_leptons)
 
@@ -31,20 +31,20 @@ def n_lep_cut(n_leptons, is_reco=False):
 #SRs["SR2b2"  ] = n_lep_cut(2, is_reco) + " && n_bjets_20 >= 2 && n_jets_25 >= 5 && met > 150 && meff >  600"
 #SRs["SR3L3b" ] = n_lep_cut(3, is_reco) + " && n_bjets_20 >= 3                   && met >  50               "
 
-SRs[     "SS/3L"] = n_lep_cut(2, is_reco=False) + " && lepton_pt[1] > 20"
-SRs[     "SR3L1"] = n_lep_cut(3, is_reco=False) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 150               "
-SRs[     "SR3L2"] = n_lep_cut(3, is_reco=False) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 200 && meff > 1500"
-SRs[     "SR0b1"] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 == 0 && n_jets_25 >= 6 && met > 150 && meff >  500"
-SRs[     "SR0b2"] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 == 0 && n_jets_40 >= 6 && met > 150 && meff >  900"
-SRs[     "SR1b" ] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 >= 1 && n_jets_25 >= 6 && met > 200 && meff >  650"
-SRs[     "SR3b" ] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 >= 3 && n_jets_25 >= 6 && met > 150 && meff >  600"
+SRs[     "SS3L_1j"] = n_lep_cut(2, is_reco=False) + " && n_jets_25 >= 1"
+SRs[       "SR3L1"] = n_lep_cut(3, is_reco=False) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 150               "
+SRs[       "SR3L2"] = n_lep_cut(3, is_reco=False) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 200 && meff > 1500"
+SRs[       "SR0b1"] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 == 0 && n_jets_25 >= 6 && met > 150 && meff >  500"
+SRs[       "SR0b2"] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 == 0 && n_jets_40 >= 6 && met > 150 && meff >  900"
+SRs[       "SR1b" ] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 >= 1 && n_jets_25 >= 6 && met > 200 && meff >  650"
+SRs[       "SR3b" ] = n_lep_cut(2, is_reco=False) + " && n_bjets_20 >= 3 && n_jets_25 >= 6 && met > 150 && meff >  600"
 
-SRs_reco["SS/3L"] = n_lep_cut(2, is_reco=True ) + " && lep_pt[1] > 20"
-SRs_reco["SR3L1"] = n_lep_cut(3, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 150               "
-SRs_reco["SR3L2"] = n_lep_cut(3, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 200 && meff > 1500"
-SRs_reco["SR0b1"] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_25 >= 6 && met > 150 && meff >  500"
-SRs_reco["SR0b2"] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_40 >= 6 && met > 150 && meff >  900"
-SRs_reco["SR1b" ] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 >= 1 && n_jets_25 >= 6 && met > 200 && meff >  650"
-SRs_reco["SR3b" ] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 >= 3 && n_jets_25 >= 6 && met > 150 && meff >  600"
+SRs_reco["SS3L_1j"] = n_lep_cut(2, is_reco=True ) + " && n_jets_25 >= 1"
+SRs_reco[  "SR3L1"] = n_lep_cut(3, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 150               "
+SRs_reco[  "SR3L2"] = n_lep_cut(3, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_40 >= 4 && met > 200 && meff > 1500"
+SRs_reco[  "SR0b1"] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_25 >= 6 && met > 150 && meff >  500"
+SRs_reco[  "SR0b2"] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 == 0 && n_jets_40 >= 6 && met > 150 && meff >  900"
+SRs_reco[  "SR1b" ] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 >= 1 && n_jets_25 >= 6 && met > 200 && meff >  650"
+SRs_reco[  "SR3b" ] = n_lep_cut(2, is_reco=True ) + " && n_bjets_20 >= 3 && n_jets_25 >= 6 && met > 150 && meff >  600"
 
 
