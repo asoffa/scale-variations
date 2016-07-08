f029fdb3ecd5340f480bcf123c0bfeec5cae1aca
Aaron Soffa <asoffa@uci.edu>
2016-07-08 06:41:11 -0700

    add CSV option; clean up

--- scale_variations/tabulate.py
+++ scale_variations/tabulate.py
@@ -1,36 +1,45 @@
 
+from __future__ import print_function
 from textwrap import dedent
 from config import DEFAULT_COLUMN_WIDTH
 
+USE_CSV = False
 
 def add_hline():
-    print "\\hline"
+    if not USE_CSV:
+        print("\\hline")
 
 def print_header(name, n_columns):
-    print dedent("""
-    """+name+"""
-    ----------
+    if not USE_CSV:
+        print(dedent("""
+        """+name+"""
+        ----------
 
-    \\begin{table}
-    \\tiny
-    \\begin{tabular}{"""+(n_columns*"l")+"""}
-    \\hline""")
+        \\begin{table}
+        \\tiny
+        \\begin{tabular}{"""+(n_columns*"l")+"""}
+        \\hline"""))
 
 def print_footer():
-    print dedent("""\
-    \\hline
-    \\end{tabular}
-    \\end{table}
+    if not USE_CSV:
+        print(dedent("""\
+        \\hline
+        \\end{tabular}
+        \\end{table}
 
-    """)
+        """))
 
 def print_table_cell(content, is_last=False, width=DEFAULT_COLUMN_WIDTH, fmt=""):
-    delimiter = "\\\\" if is_last else "&"
-    to_print = (" {:<"+str(width)+fmt+"} {} ").format(content, delimiter)
-    if is_last:
-        print to_print
+    if USE_CSV:
+        delimiter = "" if is_last else ","
+        to_print = ("{:"+fmt+"}{}").format(content, delimiter)
     else:
-        print to_print,
+        delimiter = "\\\\" if is_last else "&"
+        to_print = (" {:<"+str(width)+fmt+"} {} ").format(content, delimiter)
+    if is_last:
+        print(to_print)
+    else:
+        print(to_print, end="")
     # reset default values
     width=DEFAULT_COLUMN_WIDTH
     fmt=""
